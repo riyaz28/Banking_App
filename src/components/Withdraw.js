4d64b233f2fb67648dbaa348a1c31078508a1d8a
Dhakshayani Garapati <50317581+gsvdhakshayani@users.noreply.github.com>
2022-11-09 15:50:15 +0530

    Adding Deposit and Withdraw pages

---
+++ src/components/Withdraw.js
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { Notif } from "./Notif";
+import { formatNumber, findAccount, transact, trim, capitalize } from "./UtilityFunctions";
+
+const Withdraw = (props) => {
+    const users = JSON.parse(localStorage.getItem('users'));
+    const setNotif = props.setNotif;
+    const notif = props.notif;
+    const [accounts, setAccounts] = useState(users);
+    const [selectedAccount, setSelectedAccount] = useState({balance: 0});
+    const [depositAmount, setDepositAmount] = useState(0);
+
+    const options = accounts.map(user => {
+        return <option value={user.number}>{user.fullname} #{user.number}</option>
+    });
+
+    const displayBalance = (e) => {
+        setNotif(notif);
+        const selectedNumber = e.target.value;
+        
+        for(const user of accounts) {
+            if(user.number === selectedNumber) {
+                setSelectedAccount(user);
+                break;
+            }
+        }
+    }
+
+    const onDeposit = (e) => {
+        const amount = formatNumber(trim(e.target.value));
+        setDepositAmount(amount);
+    }
+
+    const processTransfer = (e) => {
+        e.preventDefault();
+        const amount = trim(e.target.elements.amount.value);
+        const accountNumber = e.target.elements.account.value;
+
+        if(amount > 0 && accountNumber !== "0") {
+            for(const user of accounts) {
+                if(user.number === accountNumber) {
+                    transact(user.number, amount, props.type, props.setUsers);
+                    setSelectedAccount(findAccount(user.number));
+                    setAccounts(JSON.parse(localStorage.getItem('users')));
+                    setDepositAmount(0);
+                    setNotif({message: `${capitalize(props.page)} successful.`, style: 'success'});
+                    break;
+                }
+            }
+        } 
+        else {
+            setNotif({message: `${capitalize(props.page)} failed.`, style: 'danger'});
+        }
+    }
+    // 'bx bx-up-arrow-alt'
+    const icon = 'bx bx-down-arrow-alt';
+
+    return (
+        <section id="main-content">
+            <form id="form" onSubmit={processTransfer}>
+                <h1>Withdraw</h1>
+                <Notif message={notif.message} style={notif.style} />
+                <label>Account</label>
+                <select name="account" onChange={displayBalance}>
+                    <option value="0">Select Account</option>
+                    {options}
+                </select>
+
+                <label>Current balance</label>
+                <input type="text" className="right" value={formatNumber(selectedAccount.balance)} disabled />
+                
+                <div className="transfer-icon"><i className={icon}></i></div>
+                <label>Amount to Withdraw</label>
+                <input type="text" name="amount" value={depositAmount} onChange={onDeposit} autoComplete="off" className="right big-input" />
+                <button type="submit" className="btn">Withdraw</button>
+            </form>
+        </section>
+    )
+}
+
+export default Withdraw;
